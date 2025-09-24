51f19b9fcd4f061adc7e0f27f81c31b28d51fd5a
0d00 <ycy20041108@qq.com>
2025-09-24 21:03:26 +0800

    initcommit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("姓名: ");
+        String name = scanner.nextLine();
+        System.out.print("地址: ");
+        String address = scanner.nextLine();
+        System.out.print("电话");
+        String phone = scanner.nextLine();
+
+    }
+}
