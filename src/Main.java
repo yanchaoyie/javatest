624565cd888b08e70d37ca625b26837d8233e6ff
0d00 <ycy20041108@qq.com>
2025-10-17 14:43:43 +0800

    initcommit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
-import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("姓名: ");
-        String name = scanner.nextLine();
-        System.out.print("地址: ");
-        String address = scanner.nextLine();
-        System.out.print("电话");
-        String phone = scanner.nextLine();
+import java.util.Properties;
 
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public  class Main {
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        //增
+        properties.put("1","ycy");
+        properties.put("2","sora");
+        properties.put("3","jack");
+        properties.put("3","tom");
+        properties.put("5","lucy");
+        //删
+        properties.remove("5");
+        //改，利用put方法key值相同会替代
+        properties.put("3","lucy");
+        //查，通过key值得到 value
+        System.out.println(properties.get("3"));
+        System.out.println(properties.getProperty("5"));
     }
 }
+
+
+
