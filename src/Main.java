051ea7dbe5f1a99782182eb80123c622f7f71990
0d00 <ycy20041108@qq.com>
2025-10-20 16:52:42 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,89 @@
-import java.util.Properties;
+import java.util.*;
 
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public  class Main {
-    @SuppressWarnings("all")
+public class Main {
     public static void main(String[] args) {
-        Properties properties = new Properties();
-        //增
-        properties.put("1","ycy");
-        properties.put("2","sora");
-        properties.put("3","jack");
-        properties.put("3","tom");
-        properties.put("5","lucy");
-        //删
-        properties.remove("5");
-        //改，利用put方法key值相同会替代
-        properties.put("3","lucy");
-        //查，通过key值得到 value
-        System.out.println(properties.get("3"));
-        System.out.println(properties.getProperty("5"));
+        DAO<User> userDAO = new DAO<>();
+        userDAO.save("1", new User(1, "Tom", 12));
+        userDAO.save("2", new User(2, "Mike", 13));
+        userDAO.save("3", new User(3, "Lucy", 14));
+        System.out.println(userDAO.get("1"));
+        userDAO.upDate("1", new User(1, "Sora", 15));
+        System.out.println(userDAO.get("1"));
+        System.out.println(userDAO.list());
+        userDAO.delete("3");
+        System.out.println(userDAO.list());
     }
 }
 
+class User{
+    private int id;
+    private String name;
+    private int age;
 
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+class DAO<T>{
+    Map<String,T> map = new HashMap<>();
+
+    public void save(String id,T entity){
+        map.put(id,entity);
+    }
+
+    public T get(String id){
+        return map.get(id);
+    }
+
+    public void upDate(String id, T entity){
+        map.put(id,entity);
+    }
+
+    public List<T> list(){
+        List<T> list = new ArrayList<>();
+        for (String key : map.keySet()) {
+            list.add(map.get(key));
+        }
+        return list;
+    }
+
+    public void delete(String id){
+        map.remove(id);
+    }
+}
---
+++ src/com/ycy/extends_/Computer.java
@@ -0,0 +1,10 @@
+package com.ycy.extends_;
+
+public class Computer {
+    public String cpu;
+    public String ram;
+    public String disk;
+    public void getDetils(){
+
+    }
+}
